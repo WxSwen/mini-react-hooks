a00f4575fc3690883d8159cb42be48c1a74fd2c9
WxSwen <511854671@qq.com>
2019-08-02 14:18:34 +0800

    feat: 添加useHover

--- src/package/useUpdateEffect.ts
+++ src/package/useUpdateEffect.ts
@@ -14,3 +14,28 @@ const useUpdateEffect: typeof useEffect = (effect, deps) => {
 }
 
 export default useUpdateEffect;
+
+// example
+// const Demo = () => {
+//   const [count, setCount] = React.useState(0);
+
+//   React.useEffect(() => {
+//     const interval = setInterval(() => {
+//       setCount(count => count + 1)
+//     }, 1000)
+
+//     return () => {
+//       clearInterval(interval)
+//     }
+//   }, [])
+
+//   useUpdateEffect(() => {
+//     console.log('count', count) // will only show 1 and beyond
+
+//     return () => { // *OPTIONAL*
+//       // do something on unmount
+//     }
+//   }) // you can include deps array if necessary
+
+//   return <div>Count: {count}</div>
+// };
