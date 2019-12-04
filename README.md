d317a2737867b26834afa5b1e728d3403b516a59
WxSwen <511854671@qq.com>
2019-12-04 19:09:37 +0800

    feat: 修改md，添加useFetch

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# mini-react-hooks &middot; [![GitHub license](https://img.shields.io/badge/license-ISC-blue.svg)](https://github.com/facebook/react/blob/master/LICENSE) [![npm version](https://img.shields.io/npm/v/react.svg?style=flat)](https://www.npmjs.com/package/react) [![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg)](https://reactjs.org/docs/how-to-contribute.html#your-first-pull-request)
+# mini-react-hooks &middot; [![npm version](https://img.shields.io/npm/v/mini-react-hooks.svg?style=flat)](https://www.npmjs.com/package/mini-react-hooks)
 
 React use hooks.
 
---
+++ src/package/useFetch.ts
@@ -0,0 +1,24 @@
+import { useState, useEffect, useRef } from "react";
+
+export default opts => {
+  let { initData, initLoading = true, fetchFn } = opts;
+  let [data, setData] = useState(initData);
+  let [isLoading, setIsLoading] = useState(initLoading);
+  let [isError, setIsError] = useState(false);
+  let fetchApi = useRef(fetchFn);
+
+  useEffect(() => {
+    let fetchData = async () => {
+      try {
+        let data = await fetchApi.current();
+        setData(data);
+        setIsLoading(!isLoading);
+      } catch (err) {
+        setIsError(true);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return [{ data, isLoading, isError }];
+};
